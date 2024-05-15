1b6411b007785ddaa128bfd0c01a9a6357784dda
LautaroVelez <lautivelez28@gmail.com>
2024-05-15 15:20:03 -0300

    tools and technologies added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -47,6 +47,50 @@ const LandingPage = () => {
                     </Row>
                 </Container>
             </section>
+
+            <span className={''}><h1
+                className={'m-20 text-8xl text-center text-black FontRoboto'}>TECHNOLOGIES & TOOLS</h1></span>
+
+            <Container fluid className={'font-bold text-2xl black'}>
+                <Row className={'FontRoboto'}>
+                    <Col>
+                        <h1 className={'text-start'}>TECHNOLOGIES</h1>
+                    </Col>
+                    <Col>
+                        <h1 className={'text-center'}>&</h1>
+                    </Col>
+                    <Col>
+                        <h1 className={'text-end'}>TOOLS</h1>
+                    </Col>
+                </Row>
+
+
+                <Row className={'text-center'}>
+                    <Col md={6}>
+
+                        <h1 className={'text-end'}>FRAMER MOTION</h1>
+                        <h1 className={'text-end'}>RESPONSIVE DESIGN</h1>
+                        <h1 className={'text-end'}>STORYTELLING</h1>
+                        <h1 className={'text-end'}>CSS</h1>
+                        <h1 className={'text-end'}>HMLT5</h1>
+                        <h1 className={'text-end'}>JAVASCRIPT</h1>
+                        <h1 className={'text-end'}>REACT</h1>
+
+
+                    </Col>
+                    <Col md={6}>
+                        <h1 className={'text-start'}>BOOSTRAP</h1>
+                        <h1 className={'text-start'}>TAILWIND</h1>
+                        <h1 className={'text-start'}>FIGMA</h1>
+                        <h1 className={'text-start'}>PYTHON</h1>
+                        <h1 className={'text-start'}>MATERIAL DESIGN</h1>
+                        <h1 className={'text-start'}>MUI</h1>
+                        <h1 className={'text-start'}>NEXT.UI</h1>
+                    </Col>
+                </Row>
+            </Container>
+
+
         </>
     );
 };
