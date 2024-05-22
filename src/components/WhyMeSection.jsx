c14417d218b068e9a116d4aaa8648076c2080b5a
LautaroVelez <lautivelez28@gmail.com>
2024-05-22 01:28:48 -0300

    why me animation almost

--- src/components/WhyMeSection.jsx
+++ src/components/WhyMeSection.jsx
@@ -1,14 +1,30 @@
 import {Col, Container, Row} from "react-bootstrap";
 import {FaReact} from "react-icons/fa";
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import {motion, useScroll, useTransform} from "framer-motion";
 
 const WhyMeSection = () => {
     const MiniTitleWhyMe = "I THINK THAT MY WORK CAN SPEAK FOR ME."
-    const firstParagraph = "I’m a disciplined person, who number one interest is the productivity and creative development of me and the brand im working with."
-    const secondParagraph = "I think i act like a connector, problem-solver and forward-thinker, able to impact every step of the process with excellence."
-    const thirdParagraph = "There’s no greater joy than seeing an idea come to life."
-    const fourthParagraph = "This is your baby."
-    const fifthParagraph = "I’d love to help you raise it."
+
+    const combinedParagraph = "I’m a disciplined person, who number one interest is the productivity and creative development of me and the brand I’m working with.\n" +
+        "I think I act like a connector, problem-solver and forward-thinker, able to impact every step of the process with excellence.\n" +
+        "There’s no greater joy than seeing an idea come to life.\n" +
+        "This is your baby.\n" +
+        "I’d love to help you raise it.";
+
+
+
+
+    const words = combinedParagraph.split("");
+
+    const element = useRef(null);
+
+
+    const {scrollYProgress} = useScroll({
+        target: element,
+        offset: ['start 0.9', 'start 0.25']
+    })
+
 
     return (
         <>
@@ -18,16 +34,23 @@ const WhyMeSection = () => {
                 <Container>
                     <Row className={'justify-center'}>
                         <Col md={8} className={'text-center'}>
-                            <p className={'paragraphWhyMe'}>{firstParagraph}</p>
-                            <p className={'paragraphWhyMe'}>{secondParagraph}</p>
-                            <p className={'paragraphWhyMe'}>{thirdParagraph}</p>
-                            <p className={'paragraphWhyMe'}>{fourthParagraph}</p>
-                            <p className={'paragraphWhyMe'}>{fifthParagraph}</p>
+                            <p ref={element}>{
+                                words.map((word, i) => {
+                                    const start = i / words.length;
+                                    const end = start + (1 / words.length)
+                                    return (
+                                        <Word key={i} range={[start, end]}
+                                              progress={scrollYProgress}>{word}</Word>
+
+
+                                    )
+                                })
+                            }</p>
+
                         </Col>
                     </Row>
                 </Container>
                 <hr className="HorizontalLine mt-6"/>
-
                 <Row className={'justify-center'}>
                     <FaReact className={'IconReact'}/>
                 </Row>
@@ -37,3 +60,13 @@ const WhyMeSection = () => {
 }
 
 export default WhyMeSection;
+
+const Word = ({children, range, progress}) => {
+    const opacity = useTransform(progress, range, [0, 1])
+    return (
+        <span className={'paragraphWhyMe'}>
+            <span className={'Shadow'}>{children}</span>
+            <motion.span style={{opacity}}>{children}</motion.span>
+        </span>
+    )
+}
