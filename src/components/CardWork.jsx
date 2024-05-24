046ce4ffcb2a93ed468ac6b17328f97375b2c9dc
LautaroVelez <lautivelez28@gmail.com>
2024-05-24 13:39:12 -0300

    cards styled

--- src/components/CardWork.jsx
+++ src/components/CardWork.jsx
@@ -1,7 +1,14 @@
 import '../assets/styles/CardWork.css'
 import {Col, Row} from "react-bootstrap";
 import {motion, useScroll, useTransform} from 'framer-motion';
-import {useRef} from "react";
+import React, {useRef} from "react";
+import {FaCss3, FaFigma, FaHtml5, FaReact} from "react-icons/fa";
+
+
+import {TbBrandFramerMotion} from "react-icons/tb";
+import {SiGreensock} from "react-icons/si";
+import {IoLogoJavascript} from "react-icons/io";
+import {MdArrowOutward} from "react-icons/md";
 
 
 export const CardWork = ({i, title, description, src, link, progress, range, color, targetScale}) => {
@@ -9,30 +16,54 @@ export const CardWork = ({i, title, description, src, link, progress, range, col
     const {scrollYProgress} = useScroll({
         target: container,
         offset: ["start end", 'start start']
-
     })
 
     const Imgscale = useTransform(scrollYProgress, [0, 1], [2, 1])
     const scale = useTransform(progress, range, [1, targetScale])
 
     return (
-        <div ref={container} className={'cardWorkContainer'}>
-            <motion.div  style={{backgroundColor: color, scale, top:`calc(-5vh + ${i * 25}px)`}}  className={'cardWork'}>
-                <h1 className={'text-5xl text-center font-bold'}>{title}</h1>
-                <Row>
-                    <Col md={6} className={'text-center align-middle'}>
-                        <p>{description}</p>
 
-                    </Col>
-                    <Col md={6}>
-                        <div className={'imgCardWork'}>
-                            <motion.div className={'inner'} style={{scale: Imgscale}}>
-                                <img src={`/portfolio/src/assets/images/${src}`} alt="image"/>
-                            </motion.div>
+        <div ref={container} className={'cardWorkContainer'}>
+            <motion.div style={{
+                backgroundColor: color,
+                scale,
+                top: `calc(-5vh + ${i * 25}px)`
+            }} className={'cardWork'}>
+
+                <div className={'imgCardWork'}>
+                    <a href={link}>
+                        <div className={'titleContainer'}>
+                            <h1 className={'title'}>{title}</h1>
+                            <p className={'pCardWork'}>TECHNOLOGIES USED</p>
+                            <div className={'iconsRow'}>
+                                <FaReact className={'IconsCardWork'} alt={'React'}/>
+                                <FaFigma className={'IconsCardWork'}/>
+                                <TbBrandFramerMotion className={'IconsCardWork'}/>
+                                <SiGreensock className={'IconsCardWork'}/>
+                                <FaHtml5 className={'IconsCardWork'}/>
+                                <FaCss3 className={'IconsCardWork'}/>
+                                <IoLogoJavascript className={'IconsCardWork'}/>
+                            </div>
+
                         </div>
-                    </Col>
-                </Row>
+                        <div className={'viewProject'}>
+
+
+                            <p>VIEW PROJECT</p>
+
+                            <MdArrowOutward className={'arrowIcon'}/>
+
+
+                        </div>
+                        <motion.div className={'inner'} style={{scale: Imgscale}}>
+                            <img src={`/portfolio/src/assets/images/${src}`} alt="image"/>
+                        </motion.div>
+                    </a>
+                </div>
+
             </motion.div>
         </div>
+
+
     )
 }
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,20 +1,21 @@
- const projects = [
+const projects = [
     {
-        title: "Finanzas Moviles",
+        title: "SUICIDAL CARE",
+        description: "A web designed to help people with suicidal thoughts and provide care and information to those who need it.",
+        src: "Suicidal.png",
+        link: "https://www.youtube.com/watch?v=9WlkNyIZ5Yo&ab_channel=iosonomauri",
+        color: "#D9D9D9"
+    },
+    {
+        title: "MINISTRY OF FINANCE",
         description: "Aplicacion creada para el ministerio de finanzas de cordoba, el objetivo de esta aplicacion es poder organizar visitas a diferentes localidades como grupo, poder formar, editar y eliminar grupos. Crear visitas, editarlas y verlas, y poder asignarle roles a los usuarios autenticados",
         src: "landingfinanciation.jpeg",
         link: "holaaa",
-        color: "#0080ff"
+        color: "#59adff"
     },
+
     {
-        title: "Suicidal Page",
-        description: "Aplicacion creada para ayudar a personas con pensamientos suicidas y dar atencion e informacion a quien necesite.",
-        src: "hola",
-        link: "holaaa",
-        color: "#2c2cf4"
-    },
-    {
-        title: "Menor Consumo",
+        title: "LOWER CONSUME",
         description: "Aplicacion creada para poder comparar precios y planes para personas que quieran cambiar de empresa de suministro de gas o electricidad.",
         src: "hola",
         link: "holaaa",
@@ -22,4 +23,4 @@
     }
 ]
 
- export default projects;
+export default projects;
