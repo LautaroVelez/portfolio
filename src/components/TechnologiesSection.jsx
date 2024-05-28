8d8a75510886fdd66c62b16db321834ff0959441
LautaroVelez <lautivelez28@gmail.com>
2024-05-28 02:16:43 -0300

    big text animated technologies section

--- src/components/TechnologiesSection.jsx
+++ src/components/TechnologiesSection.jsx
@@ -1,22 +1,50 @@
 import '../assets/styles/Technologies.css';
+import {useLayoutEffect, useRef} from "react";
+import gsap from 'gsap';
+import {ScrollTrigger} from "gsap/ScrollTrigger";
+import {motion, useInView, useScroll, useTransform} from "framer-motion";
 
 
 const TechnologiesSection = () => {
+
+    const word = useRef(null);
+    const word2 = useRef(null);
+    const word3 = useRef(null);
+
+
+    const {scrollYProgress} = useScroll({
+        target: word, word2, word3,
+        offset: ["0 60vh", "end 50vh"],
+    });
+
+    const scale1 = useTransform(scrollYProgress, [0, 1], [3, 0.35]);
+    const x1 = useTransform(scrollYProgress, [0, 1], ["80%", "-35%"]);
+
+    const scale2 = useTransform(scrollYProgress, [0, 1], [3, 0.35]);
+    const x2 = useTransform(scrollYProgress, [0, 1], ["52%", "-32%"]);
+
+    const scale3 = useTransform(scrollYProgress, [0, 1], [3, 0.35]);
+    const x3 = useTransform(scrollYProgress, [0, 1], ["-80%", "30%"]);
+
     return (
 
         <section id={'Technologies'} className={' h-[140vh] FontSection'}>
 
 
-            <div className={'grid grid-cols-3 grid-rows-1 z-3 sticky top-50 justify-between'}>
-                <h1 className={'SmallText text-start ml-10'}>TECHNOLOGIES</h1>
-                <h1 className={'SmallText text-center'}>&</h1>
-                <h1 className={'SmallText text-end mr-10'}>TOOLS</h1>
+            <div className={'grid grid-cols-3 grid-rows-1 z-3 sticky top-50 justify-between mt-20'}>
+
+                <motion.div style={{scale: scale1, x: x1,}}>
+                    <h1 ref={word} className={'SmallText text-start ml-10'}>TECHNOLOGIES</h1>
+                </motion.div>
+                <motion.div style={{scale: scale2, x: x2}}>
+                    <h1 className={'SmallText text-center'}>&</h1>
+                </motion.div>
+                <motion.div style={{scale: scale3, x: x3}}>
+                    <h1 className={'SmallText text-end mr-10'}>TOOLS</h1>
+                </motion.div>
             </div>
 
-            <div>
-                <h1 className={'text-center BigText'}>TECHNOLOGIES & TOOLS</h1>
-            </div>
-            <div className={'text-center SmallText'}>
+            <div className={'text-center SmallText flex flex-col justify-center h-full'}>
 
 
                 <h1>JAVASCRIPT</h1>
