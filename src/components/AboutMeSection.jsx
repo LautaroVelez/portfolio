c42fa58da5b55e484de3cf75a535e5dc1752e7d5
LautaroVelez <lautivelez28@gmail.com>
2024-05-27 22:19:56 -0300

    new about me

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -1,10 +1,10 @@
 import {motion, useScroll, useTransform} from "framer-motion";
-import me from "../assets/images/me.jpg";
-import React, {useRef} from "react";
+import  {useRef} from "react";
 import {Col, Row} from "react-bootstrap";
 import '/src/assets/styles/LandingPage.css';
 import {isMobile} from "react-device-detect";
 import mewhitoutfondo from "../assets/images/mewhitoutfondo.png";
+import meexpand from "../assets/images/meexpand.jpg";
 
 const AboutMeSection = () => {
     const FirstRef = useRef(null);
@@ -26,56 +26,68 @@ const AboutMeSection = () => {
         offset: ["start end", "end start"],
     });
 
-    const x1 = useTransform(scrollYProgress1, [0, 1], ["-25%", "5%"]);
-    const x2 = useTransform(scrollYProgress2, [0, 1], ["10%", "-90%"]);
-
-    const x3 = useTransform(scrollYProgress3, [0, 1], ["5%", "-250%"]);
+    const x1 = useTransform(scrollYProgress1, [0, 1], ["-10%", "20%"]);
+    const x2 = useTransform(scrollYProgress2, [0, 1], ["20%", "-20%"]);
+    const x3 = useTransform(scrollYProgress3, [0, 2], ["0%", "-180%"]);
 
     return (
+
+
         <section className={'AboutMeSection md:h-[140vh]'}>
-            <Row>
-                <Col md={9} sm={12}>
+            <div className='h-full w-full relative grid place-items-center overflow-hidden '>
+                <div className={'absolute inset-0 z-0 h-full'} style={{
+                    backgroundImage: {meexpand},
+                    backgroundSize: "cover",
+
+                }}>
+                    <img src={meexpand} alt="" className={'h-full  w-full bg-cover object-cover'}/>
+                </div>
+                <div className={'absolute inset-0 z-3 h-[140vh]'} style={{
+                    backgroundImage: {mewhitoutfondo},
+                    backgroundSize: "cover",
+                }}>
+                    <img src={mewhitoutfondo} alt="" className={'h-full w-full object-cover'}/>
+                </div>
+                <div className={'text-start align-top row flex col-12 left-0 z-2'}>
                     {isMobile ? (
-                        <h1 className="AboutMeTitle text-center ColorBlack">ABOUT</h1>
+                        <h1 className="AboutMeTitle  ColorBlack">ABOUT</h1>
                     ) : (
                         <motion.div ref={FirstRef} style={{x: x1}}>
-                            <motion.h1 className="AboutMeTitle  text-center ColorBlack">ABOUT</motion.h1>
+                            <h1 className="AboutMeTitle ColorBlack ">ABOUT</h1>
                         </motion.div>)}
-                </Col>
-                <Col md={12} sm={12}>
+                </div>
+                <div className={'z-2 text-center row flex col-12 -mt-48'}>
                     {isMobile ? (
                         <h1 className="AboutMeTitle text-center ColorWhite">ME</h1>
                     ) : (
                         <motion.div ref={SecondRef} style={{x: x2}}>
-                            <motion.h1 className="AboutMeTitle text-end ColorWhite">ME</motion.h1>
+                            <h1 className="AboutMeTitle ColorWhite">ME</h1>
                         </motion.div>
                     )}
-                </Col>
-            </Row>
-            <Row className={''}>
-                <Col md={6}>
-                </Col>
-                <Col sm={12} md={6}>
+                </div>
+                <div className={'justify-center flex col-12 z-2 -mt-48'}>
                     <motion.div ref={ThirdRef} style={{x: x3}} className={'MeTextContainer'}>
-
                         <h1 className="MeText FontKaisei">Hello there! I’m Lautaro Velez, a web developer borned in
                             Córdoba,
                             Argentina.</h1>
-
                         <h1 className="MeText FontEmbed mt-2">
                             I have a solid knowledge in React, JavaScript, HTML, CSS, and more.
                         </h1>
                         <h1 className="MeText FontKaisei mt-2">
-                            I consider myself a very creative and a hard-working person who is always trying to learn
+                            I consider myself a very creative and a hard-working person who is always trying to
+                            learn
                             something new.
                         </h1>
-                        <h1 className="MeText FontKaisei mt-2">Extra: I'm 19 years old, I love working out, cars, music,
+                        <h1 className="MeText FontKaisei mt-2">Extra: I'm 19 years old, I love working out, cars,
+                            music,
                             and watching rom-coms!</h1>
                     </motion.div>
-                </Col>
-            </Row>
+                </div>
+            </div>
         </section>
-    );
+    )
+        ;
 };
+
 
 export default AboutMeSection;
