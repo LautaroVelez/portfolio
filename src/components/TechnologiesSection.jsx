7f3d9f226eb09b01730c6923978a068403bbbe68
LautaroVelez <lautivele28@gmail.com>
2024-05-31 17:21:39 -0300

    styled section technologies

--- src/components/TechnologiesSection.jsx
+++ src/components/TechnologiesSection.jsx
@@ -3,6 +3,9 @@ import { useRef, useEffect, useState } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 
 const TechnologiesSection = () => {
+
+    //calculo de animacion de repulsion
+    //agarro la posicion del scroll y el top de la section
     const { scrollY } = useScroll();
     const sectionRef = useRef(null);
     const [sectionTop, setSectionTop] = useState(0);
@@ -13,6 +16,7 @@ const TechnologiesSection = () => {
             setSectionTop(rect.top + window.scrollY);
         }
     }, [sectionRef]);
+
 
     const calculateRepulsion = (baseX, baseY, scrollY) => {
         const distanceY = (baseY-75) - ((scrollY+50) - sectionTop);
@@ -40,19 +44,40 @@ const TechnologiesSection = () => {
         { x: 150, y: 300 },
     ];
 
+
+    const word = useRef(null);
+    const word2 = useRef(null);
+    const word3 = useRef(null);
+
+
+    const {scrollYProgress} = useScroll({
+        target: word, word2, word3,
+        offset: ["0 60vh", "end 50vh"],
+    });
+
+    const scale1 = useTransform(scrollYProgress, [0,0.50, 1], [3, 0.30,3]);
+    const x1 = useTransform(scrollYProgress, [0, 0.50,1], ["80%", "-35%","80%"]);
+
+    const scale2 = useTransform(scrollYProgress, [0, 0.50,1], [3, 0.30, 3]);
+    const x2 = useTransform(scrollYProgress, [0, 0.50,1], ["52%", "-18%", "52%"]);
+
+    const scale3 = useTransform(scrollYProgress, [0, 0.50,1], [3, 0.30, 3]);
+    const x3 = useTransform(scrollYProgress, [0, 0.50,1], ["-80%", "30%", "-80%"]);
+
     return (
-        <section id='Technologies' className='h-[140vh] FontSection Technologies' ref={sectionRef}>
-            <div className='grid grid-cols-3 grid-rows-1 z-1 sticky top-50 justify-between mt-20'>
-                <div className='text-start'>
-                    <h1>TECHNOLOGIES</h1>
-                </div>
-                <div className='text-center'>
-                    <h1>&</h1>
-                </div>
-                <div className='text-end'>
-                    <h1>TOOLS</h1>
-                </div>
+        <section id='Technologies' className='h-[140vh] bg-[#3b4838] Technologies' ref={sectionRef}>
+            <div className='grid grid-cols-3 grid-rows-1 z-1 sticky top-50 justify-between'>
+                <motion.div style={{scale: scale1, x: x1,}}>
+                    <h1 ref={word} className={'SmallText text-start ml-10'}>TECHNOLOGIES</h1>
+                </motion.div>
+                <motion.div style={{scale: scale2, x: x2}}>
+                    <h1 className={'SmallText text-center'}>&</h1>
+                </motion.div>
+                <motion.div style={{scale: scale3, x: x3}}>
+                    <h1 className={'SmallText text-end mr-10'}>TOOLS</h1>
+                </motion.div>
             </div>
+
 
             <div className="flex items-center justify-center h-screen">
                 <div className="grid grid-cols-2 gap-4">
