30fbe3988d2b6df1bdc20a993978f85462f3528e
LautaroVelez <lautivelez28@gmail.com>
2024-06-26 01:40:35 -0300

    things to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,37 @@
 import LandingPage from "./pages/landing/Landing.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {AnimatePresence} from "framer-motion";
-import {Route, Routes} from 'react-router-dom';
+import { AnimatePresence } from "framer-motion";
+import { Route, Routes, useLocation } from 'react-router-dom';
 import ProjectInfoSuic from "./pages/ProjectInfoSuic/ProjectInfoSuic.jsx";
-import {useEffect} from "react";
+import { useEffect } from "react";
 import Lenis from "lenis";
 import ProjectInfoFinan from "./pages/ProjectInfoFinan/ProjectInfoFinan.jsx";
 import ProjectInfoCons from "./pages/ProjectInfoCons/ProjectInfoCons.jsx";
 
 export default function App() {
     useEffect(() => {
-        const lenis = new Lenis()
+        const lenis = new Lenis();
 
         function raf(time) {
-            lenis.raf(time)
-            requestAnimationFrame(raf)
+            lenis.raf(time);
+            requestAnimationFrame(raf);
         }
 
-        requestAnimationFrame(raf)
+        requestAnimationFrame(raf);
     }, []);
+
+    const location = useLocation();
 
     return (
         <div className='main'>
             <AnimatePresence mode='wait'>
                 <Routes location={location} key={location.pathname}>
-                    <Route path={'/portfolio'} index element={<LandingPage key={LandingPage}/>}/>
-                    <Route path={'/projectSuicidal'} element={<ProjectInfoSuic/>}/>
-                    <Route path={'/projectfinanciation'} element={<ProjectInfoFinan/>}/>
-                    <Route path={'/projectconstruction'} element={<ProjectInfoCons/>}/>
+                    <Route path='/' element={<LandingPage key={LandingPage} />} />
+                    <Route path='/projectSuicidal' element={<ProjectInfoSuic />} />
+                    <Route path='/projectfinanciation' element={<ProjectInfoFinan />} />
+                    <Route path='/projectconstruction' element={<ProjectInfoCons />} />
                 </Routes>
             </AnimatePresence>
         </div>
-
-    )
-        ;
+    );
 }
-
-
