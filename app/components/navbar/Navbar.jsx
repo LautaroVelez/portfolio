4d63d4e9b1f56930bceaadfa9168bd886ca43d3c
LautaroVelez <lautivelez28@gmail.com>
2024-09-04 00:51:53 -0300

    added button lenguages

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 'use client'
 import {Varela} from 'next/font/google'
-import {Button, ButtonGroup, Divider} from "@nextui-org/react";
+import {Button, ButtonGroup, Divider, Switch} from "@nextui-org/react";
+import {useState} from "react";
 
 const varela = Varela({
     weight: '400',
@@ -8,11 +9,12 @@ const varela = Varela({
 })
 
 export default function Navbar() {
+
+
     return (
         <div className={'md:sticky bg-[#171717] top-0 z-50'}>
-            <div className={'flex flex-col md:flex-row justify-between items-center p-8 text-start '}>
+            <div className={'flex flex-col md:flex-row justify-between w-full items-center p-8 text-start '}>
                 <div className={`text-start justify-start flex text-white sticky`}>
-
                     <a href="/">
                         <h1 className={`a ${varela.className} text-2xl md:text-4xl`}>LAUTARO</h1>
                     </a>
@@ -20,7 +22,7 @@ export default function Navbar() {
                 <div className={'flex items-center justify-center mt-4 md:mt-0'}>
 
                     <div className={`text-start justify-start flex text-white  sticky`}>
-                        <a href="/about" >
+                        <a href="/about">
                             <h1 className={`a ${varela.className} mx-2 text-[1rem] md:text-[1rem]`}>ABOUT</h1>
                         </a>
                     </div>
@@ -32,15 +34,17 @@ export default function Navbar() {
                     </div>
 
                     <div className={`text-start items-center justify-start flex text-white sticky`}>
-
                         <a href="/contact">
                             <h1 className={`a ${varela.className} mx-2 text-[1rem] md:text-[1rem]`}>CONTACT</h1>
                         </a>
                     </div>
-
                 </div>
-
-
+                <div className={'flex justify-center items-center text-white'}>
+                    <button className={'switch'}>
+                        <input type="checkbox" id={'switcher'}/>
+                        <label htmlFor="switcher"></label>
+                    </button>
+                </div>
             </div>
         </div>
     )
@@ -48,9 +52,6 @@ export default function Navbar() {
 
 /*
 <div className={'flex justify-end order-2 md:order-none md:mt-0 mt-4 sticky'}>
-                    <ButtonGroup radius={'full'} size={'sm'}>
-                        <Button className={'h-[5vh]  bg-black text-white'}>.ESP</Button>
-                        <Button className={'h-[5vh] bg-[#E2DFD2]'}>.ENG</Button>
-                    </ButtonGroup>
+
                 </div>
  */
